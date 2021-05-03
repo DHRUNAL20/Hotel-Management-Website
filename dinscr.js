37925f440b84e18d3a1ed9d54747fb8c84803f0d
DHRUV SHARMA <dhruv0198.be20@chitkara.edu.in>
2021-05-03 19:02:48 +0530

    Add files via upload

---
+++ dinscr.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+	$('#b').click(function(){
+		$('#bill').text(" You Ordered "+$('#c1:checked').map(function(){return this.name;}).get().join(" , "));
+
+
+	});
+	$("#b1").click(function(){
+		$("#opt").show();
+
+	});
+	$("#conf").click(function(){
+		alert("THANK YOU FOR ORDERING. YOU WILL GET YOUR ORDER SOON")
+	});
+	
+
+});
