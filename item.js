62d1c63ce09641f6cbf0975deb85e111c4cb9516
DHRUV SHARMA <dhruv0198.be20@chitkara.edu.in>
2021-05-06 19:08:14 +0530

    Add files via upload

---
+++ item.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$('#ps').click(function(){
+		$("#name").text("PUNJABI SPL.");
+
+	});
+	$('#ss').click(function(){
+		$("#name").text("SOUTH INDIAN SPL.");
+
+	});
+	$('#cs').click(function(){
+		$("#name").text("CHINESE SPL.");
+
+	});
+	$('#ts').click(function(){
+		$("#name").text("THAI FOOD SPL.");
+
+	});
+	$('#ses').click(function(){
+		$("#name").text("SEA FOOD SPL.");
+
+	});
+});
